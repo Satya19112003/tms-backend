dd03b5621b931c658df79fe3ec51177446143b24
Satya19112003 <ukalasapudi@gmail.com>
2025-12-05 17:07:28 +0530

    Update TruckAsset.java

--- src/main/java/com/cargopro/tms/entity/TruckAsset.java
+++ src/main/java/com/cargopro/tms/entity/TruckAsset.java
@@ -1,15 +1,24 @@
 package com.cargopro.tms.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 @Entity
-@Data
 public class TruckAsset {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     private String truckType;
-    private int count; // Number of available trucks of this type
+    private int count;
+
+    // --- Manual Getters and Setters ---
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTruckType() { return truckType; }
+    public void setTruckType(String truckType) { this.truckType = truckType; }
+
+    public int getCount() { return count; }
+    public void setCount(int count) { this.count = count; }
 }
